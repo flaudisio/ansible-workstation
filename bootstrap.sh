4600c6254e8e9f491375d729f8c6c29f0d2760ab
Flaudísio Tolentino <flaudisio@gmail.com>
2017-05-24 22:05:13 -0300

    Bootstrap: instala Ansible, se necessário

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-sudo apt update
+main()
+{
+    sudo apt update
 
-sudo apt install --no-install-recommends --yes \
-    ca-certificates \
-    curl \
-    python \
-    python-apt
+    sudo apt install --no-install-recommends --yes \
+        ca-certificates \
+        curl \
+        python \
+        python-apt \
+        software-properties-common
+
+    if ! dpkg-query --show ansible ; then
+        sudo add-apt-repository --yes ppa:ansible/ansible
+        sudo apt update
+        sudo apt install --no-install-recommends ansible
+    fi
+
+    ansible --version
+}
+
+echo "Starting in 3 seconds..."
+echo
+
+(
+    set -x
+    sleep 3
+    main
+)
+
+echo
+echo "Done!"
